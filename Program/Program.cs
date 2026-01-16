d2622e489308f02d5f299fdf170854d502d70e75
vikatkacenko35-maker <vikatkacenko35@gmail.com>
2026-01-16 14:14:51 +0300

    "add line: дополнительное сообщение в консоль"

--- Program/Program.cs
+++ Program/Program.cs
@@ -7,5 +7,6 @@ class Program
         Console.WriteLine("Enter your name:");
         string? name = Console.ReadLine();
         System.Console.WriteLine($"Helo {name}");
+        System.Console.WriteLine("How are you?");
     }
 }
